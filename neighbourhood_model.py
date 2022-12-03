ad1747045e6f1fd6d6bc2af8747f3ee38e5414cb
Katherine Chen <katherine.chen@thetradedesk.com>
2022-12-03 21:09:02 +1100

    Use random to generate test and train datasets

--- neighbourhood_model.py
+++ neighbourhood_model.py
@@ -3,11 +3,13 @@ import csv
 import math
 import numpy as np
 import sys
+import random
+import os
 from scipy.spatial import distance
 
 def find_index_of_top_n_neighbours(neighbourhood_size, user_index, similarities):
     res = np.argsort(similarities[user_index])[-neighbourhood_size:]
-    print("The {} most similar neightbours for user {} are user {}".format(neighbourhood_size, user_index, res))
+    # print("The {} most similar neightbours for user {} are user {}".format(neighbourhood_size, user_index, res))
     return res
 
 def compute(neighbourhood_size, train_matrix):
@@ -31,32 +33,56 @@ def compute(neighbourhood_size, train_matrix):
                 predicted_rating /= temp
                 train_matrix[user_index][i] = predicted_rating
 
+def write_to_output_file(output_file_name, matrix):
+    f = open(output_file_name, 'w')
+    writer = csv.writer(f)
+    for line in matrix:
+      writer.writerow(line)
+    f.close()
+
 if __name__ == "__main__":
     train_matrix = []
     test_matrix = []
-    neighbourhood_size = 10
+    neighbourhood_size = 15
 
-    test_file = "google_review_ratings_test_small.csv" # the original file
-    train_file = "google_review_ratings_train_small.csv" # the file that has missing data
+    file_name = "google_review_ratings_test_small.csv" # the original file
+    train_dataset_file_name = "neighbourhood_train_data_before_prediction.csv"
 
-    f = open(test_file, 'r')
+    f = open(file_name, 'r')
     reader = csv.reader(f)
-    #next(reader, None)  # skip the headers
     for line in reader:
         test_matrix.append([eval(i) for i in line[1:]])
-
-    f = open(train_file, 'r')
-    reader = csv.reader(f)
-    for line in reader:
         train_matrix.append([eval(i) for i in line[1:]])
-    
     f.close()
+
+    # Output the neighbourhood_test_data.csv file
+    write_to_output_file("neighbourhood_test_data.csv", test_matrix)
+
+    if os.path.exists(train_dataset_file_name):
+        print("neighbourhood_train_data_before_prediction.csv file exists, use this dataset to train.")
+        train_matrix = []
+        f = open(train_dataset_file_name, 'r')
+        reader = csv.reader(f)
+        for line in reader:
+            train_matrix.append([eval(i) for i in line])
+        f.close()
+    else:
+        # Create train data if no train data created
+        for r in range(len(train_matrix)):
+            for c in range(len(train_matrix[0])):
+                if random.random() > 0.3:
+                    train_matrix[r][c] = 0
+        # Output the neighbourhood_train_data_before_prediction.csv file
+        write_to_output_file("neighbourhood_train_data_before_prediction.csv", train_matrix)
+
     compute(neighbourhood_size, train_matrix)
+    # Output the neighbourhood_train_data_before_prediction.csv file
+    write_to_output_file("neighbourhood_train_data_after_prediction.csv", train_matrix)
 
     rmse = 0
     for i in range(len(test_matrix)):
         for j in range(len(test_matrix[0])):
-            rmse += (train_matrix[i][j] - test_matrix[i][j]) * (train_matrix[i][j] - test_matrix[i][j])
+            rmse += (train_matrix[i][j] - test_matrix[i][j]) ** 2
         rmse /= len(train_matrix)
         rmse = math.sqrt(rmse)
     print("RMSE is {} after training using neighbourhood method with neighbourhood size {}".format(rmse, neighbourhood_size))
